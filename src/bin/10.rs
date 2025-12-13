61f47474695e06b7a55abd609659f9572def9415
plastic041 <plastic041@gmail.com>
2025-12-13 01:36:00 +0000

    10?

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -15,9 +15,10 @@ use z3::ast::Int;
 use z3::{Optimize, SatResult};
 
 #[derive(Debug, Clone)]
-pub struct Machine {
-    pub indicators: Vec<bool>,
-    pub buttons: Vec<Vec<usize>>,
+struct Machine {
+    indicators: Vec<bool>,
+    buttons: Vec<Vec<usize>>,
+    jolts: Vec<u64>,
 }
 
 fn indicator(input: &str) -> IResult<&str, bool> {
@@ -30,6 +31,10 @@ fn indicators(input: &str) -> IResult<&str, Vec<bool>> {
 
 fn usize_parser(input: &str) -> IResult<&str, usize> {
     map_res(digit1, |s: &str| s.parse::<usize>()).parse(input)
+}
+
+fn u64_parser(input: &str) -> IResult<&str, u64> {
+    map_res(digit1, |s: &str| s.parse::<u64>()).parse(input)
 }
 
 fn button(input: &str) -> IResult<&str, Vec<usize>> {
@@ -45,12 +50,21 @@ fn buttons(input: &str) -> IResult<&str, Vec<Vec<usize>>> {
     separated_list1(multispace1, button).parse(input)
 }
 
-pub fn parse_machine(input: &str) -> IResult<&str, Machine> {
+fn jolts(input: &str) -> IResult<&str, Vec<u64>> {
+    delimited(char('{'), separated_list1(char(','), u64_parser), char('}')).parse(input)
+}
+
+fn parse_machine(input: &str) -> IResult<&str, Machine> {
     map(
-        (indicators, preceded(multispace1, buttons)),
-        |(indicators, buttons)| Machine {
+        (
+            indicators,
+            preceded(multispace1, buttons),
+            preceded(multispace1, jolts),
+        ),
+        |(indicators, buttons, jolts)| Machine {
             indicators,
             buttons,
+            jolts,
         },
     )
     .parse(input)
@@ -109,6 +123,57 @@ impl Machine {
             _ => Err("No solution".to_string()),
         }
     }
+
+    fn find_min_presses2(&self) -> Result<u64, String> {
+        let optimizer = Optimize::new();
+
+        let mut button_vars = Vec::new();
+        for i in 0..self.buttons.len() {
+            let button = Int::new_const(format!("button_{}", i));
+            button_vars.push(button.clone());
+            optimizer.assert(&button.ge(&Int::from_i64(0)));
+        }
+
+        for i in 0..self.indicators.len() {
+            let mut buttons = Vec::new();
+
+            for j in 0..self.buttons.len() {
+                if self.buttons[j].contains(&i) {
+                    buttons.push(&button_vars[j]);
+                }
+            }
+
+            if !buttons.is_empty() {
+                let sum = buttons
+                    .iter()
+                    .map(|b| (*b).clone())
+                    .reduce(|acc, button| acc + button)
+                    .unwrap();
+                optimizer.assert(&sum.eq(&Int::from_u64(self.jolts[i])));
+            }
+        }
+
+        assert!(!button_vars.is_empty());
+
+        let total = button_vars
+            .iter()
+            .map(|b| (*b).clone())
+            .reduce(|acc, button| acc + button)
+            .unwrap();
+        optimizer.minimize(&total);
+
+        match optimizer.check(&[]) {
+            SatResult::Sat => {
+                let model = optimizer.get_model().unwrap();
+                let result = button_vars
+                    .iter()
+                    .map(|button| model.eval(button, true).unwrap().as_u64().unwrap())
+                    .sum();
+                Ok(result)
+            }
+            _ => Err("No solution".to_string()),
+        }
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -122,7 +187,13 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let machines = input
+        .lines()
+        .flat_map(parse_machine)
+        .map(|(_, m)| m)
+        .collect_vec();
+
+    Some(machines.iter().flat_map(Machine::find_min_presses2).sum())
 }
 
 #[cfg(test)]
